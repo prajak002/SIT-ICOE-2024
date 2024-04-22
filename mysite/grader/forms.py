adcd32287dd146e7e75a100764a435a90f1c94b8
prajak002 <66613-Prajak002@users.noreply.drupalcode.org>
2024-04-23 04:50:32 +0530

    5th

--- mysite/grader/forms.py
+++ mysite/grader/forms.py
@@ -5,7 +5,7 @@ from .models import Essay
 
 class AnswerForm(forms.ModelForm):
     answer = forms.CharField(max_length=100000, widget=forms.Textarea(
-        attrs={'rows': 20, 'placeholder':  "Your essay here"}))
+        attrs={'rows': 20, 'placeholder':  " Write Your essay here"}))
 
     class Meta:
         model = Essay
